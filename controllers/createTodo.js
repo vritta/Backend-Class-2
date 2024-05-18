1d520e37261f75b47c63caef17130856c289afd0
vritta <anuhartripathi15@gmail.com>
2024-05-18 20:54:23 +0530

    created 'createTodo' function

--- controllers/createTodo.js
+++ controllers/createTodo.js
@@ -3,4 +3,26 @@ const Todo = require("../models/Todo");
 
 //define route handler
 exports.createTodo = async(req, res){
+    try{
+        const {title, description} = req.body;
+        const response = await Todo.create({title, description});
+        res.status(200).json(
+            {
+                success:true,
+                data:response,
+                message:'Entry Created Successfully'
+            }
+        );
+    }
+    catch(err){
+        console.error(err);
+        console.log(err.message);
+        res.status(500).json(
+            {
+                success:false,
+                data:"Internal server error",
+                message:'Entry Created Successfully'
+            }
+        );
+    }
 }
